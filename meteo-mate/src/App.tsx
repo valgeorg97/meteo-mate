d557de52781b2ef7ef5476a93398cc6c31876f48
Valentin Georgiev <valentingeorgiev1997@abv.bg>
2023-07-03 16:58:50 +0300

    Initialize project with React and TypeScript

---
+++ meteo-mate/src/App.tsx
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import './App.css';
+import axios from 'axios';
+import Navigation from '../src/components/Navigation'
+import { API_KEY } from './constants/constants';
+import { ChakraProvider } from "@chakra-ui/react"
+
+
+function App() {
+
+  // const url = `https://api.openweathermap.org/data/3.0/onecall?llat=33.44&lon=-94.04&exclude=hourly,daily&appid=${API_KEY}`
+  return (
+    <ChakraProvider>
+      <Navigation></Navigation>
+    </ChakraProvider>
+  );
+}
+
+export default App;
